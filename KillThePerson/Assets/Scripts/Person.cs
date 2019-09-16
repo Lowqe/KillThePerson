bdc5202f34e0c9dd8f1a82a1ac4f22dfb2f909d8
RagnarAnders <ragner92@gmail.com>
2019-09-16 11:59:19 +0200

    Skapat en mall
    Nu har jag skapat en mall där man bara behöver lägga in bild, namn och beskrivning

---
+++ KillThePerson/Assets/Scripts/Person.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Person", menuName = "Persons")]
+public class Person : ScriptableObject
+{
+    [SerializeField] private new string name;
+    [SerializeField] private string description;
+    [SerializeField] private Sprite image;
+    public string Description { private set => description = value; get => description; }
+    public Sprite Image { private set => image = value; get => image; }
+    public string Name { get => name; private set => name = value; }
+}
---
+++ KillThePerson/Assets/Scripts/PersonDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PersonDisplay : MonoBehaviour
+{
+    [SerializeField]private Person person;
+    [SerializeField] private Text name;
+    [SerializeField] private Text description;
+    [SerializeField] private Image Image;
+    void Start()
+    {
+        name.text = person.name;
+        description.text = person.Description;
+        Image.sprite = person.Image;
+    }
+
+   
+}
